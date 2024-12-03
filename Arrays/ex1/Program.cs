f55fc216ae0cd4a49a92f1c77d6b6e422f50f6a1
José Passagnolo Neto <jose.passagnolo@forlogic.net>
2024-12-03 08:41:29 -0300

    exercício 01 arrays

---
+++ Arrays/ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] notas = new double[3];
+            double total = 0;
+
+            for (int i = 0; i < 3; i++) {
+                Console.Write($"Digite o valor da nota {i + 1}: ");
+                double nota = double.Parse(Console.ReadLine());
+                notas[i] = nota;
+                total += nota;
+            }
+
+            double media = total / notas.Length;
+            Console.WriteLine($"Média: {media}");
+        }
+    }
+}
