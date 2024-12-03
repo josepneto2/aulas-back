2da1cb61c12c61dc1a937e3ed44b1ed1a13f5ec7
José Passagnolo Neto <jose.passagnolo@forlogic.net>
2024-12-03 09:35:46 -0300

    exercício 2 arrays

---
+++ Arrays/ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ex2 {
+    internal class Program {
+        static void Main(string[] args) {
+            string[] nomes = new string[3];
+
+            for (int i = 0; i < nomes.Length; i++) {
+                Console.Write($"Digite o nome do aluno {i+1}: ");
+                nomes[i] = Console.ReadLine();
+            }
+
+            Console.Write("Digite o número do aluno que quer ver: ");
+            int numero = int.Parse(Console.ReadLine()) - 1;
+
+            Console.WriteLine(nomes[numero]);
+        }
+    }
+}
--- Arrays/ex2/obj/Debug/net8.0/ex2.AssemblyInfo.cs
+++ Arrays/ex2/obj/Debug/net8.0/ex2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ex1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e857584c087fe9436b7c74ef21e3b54f764d512f")]
-[assembly: System.Reflection.AssemblyProductAttribute("ex1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ex1")]
+[assembly: System.Reflection.AssemblyProductAttribute("ex2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
